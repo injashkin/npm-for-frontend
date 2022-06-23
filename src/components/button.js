eca0b44a9652b5a73d222ff9e9184f554a66370b
injashkin <injashkin@gmail.com>
2022-06-23 16:28:02 +0300

    Rollup is configured
    Вернулся на коммит назад. Попытка заставить работать Browserify и
    Babel, как мне нужно, не увенчались успехом. При поиске ответов
    наткнулся в сети на Rollup. Он позволил быстро себя настроить и
    запустить. Судя по описанию Rollup мне подойдет даже больше, чем
    Browserify.

---
+++ src/components/button.js
@@ -0,0 +1,5 @@
+export const button = () => {
+  let button = document.querySelector('.button');
+  button.innerHTML = 'Этот текст изменил JavaScript7';
+  document.body.appendChild(button);
+};
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { button } from './components/button.js';
+
+button();
