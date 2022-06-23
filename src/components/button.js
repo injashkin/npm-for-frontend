215acf548143f1404bb1f0412e0426ac28209524
injashkin <injashkin@gmail.com>
2022-06-23 20:06:25 +0300

    Refactor code

--- src/components/button.js
+++ src/components/button.js
@@ -1,5 +1,10 @@
 export const button = () => {
   let button = document.querySelector('.button');
-  button.innerHTML = 'Этот текст изменил JavaScript7';
-  document.body.appendChild(button);
+  let text = button.textContent;
+  const message = "Этот текст изменен JavaScript'ом";
+
+  button.addEventListener('click', () => {
+    text === message ? (text = 'Жми еще!') : (text = message);
+    button.textContent = text;
+  });
 };
